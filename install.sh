832171888a736c5071430730315a26213afafd6d
User <willnhalt@gmail.com>
2017-07-15 07:40:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,13 @@ sudo pacman -S plank
 zoom-enabled=true
 zoom-percent=115
 
+cd
+sudo mkdir /Home/$user/.themes
+
 https://codeload.github.com/B00merang-Project/macOS-Sierra/zip/master
+unzip /Downloads/macOS-Sierra-master.zip
+cp -r /Downloads/macOS-Sierra-master /.themes
+
+
+[org.gnome.desktop.wm.preferences]
+theme='macOS-Sierra-Master'
